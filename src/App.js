fedb82387b0ba93e9729053c2aad2896e0be2035
Isaqb <isaqbarbosa@gmail.com>
2021-10-03 17:20:09 -0300

    Mostrar todos os herois

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import React, { useEffect, useState} from "react";
-import api from "./service/api";
 import  "./App.css";
+import ShowAll from "./ShowAll";
 
 function App() {
-  const [hero, setHero] = useState();
-
-  useEffect(() => {
-    api
-      .get("/1")
-      .then((response) => setHero(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
-  
-  return (
-    <div className="App">
-      <p>Nome: {hero?.name}</p>
-      <img className="heroImg" src={hero?.image.url}></img>
-    </div>
+  return(
+    <ShowAll/>
   );
 }
 
---
+++ src/ShowAll.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState} from "react";
+import api from "./service/api";
+import  "./App.css";
+
+export default function ShowAll(){
+    
+    function CallApi(id){
+      const [hero, setHero] = useState();
+      useEffect(() => {
+        api
+          .get(id)
+          .then((response) => setHero(response.data))
+          .catch((err) => {
+            console.error("Ocorreu um erro:" + err);
+          });
+      }, []);
+      return hero;
+    }
+    
+    const listHeroes = [];
+
+    for(let i= 1 ; i<=731;i++){
+      let r = CallApi(i.toString())
+      if(r !== undefined){
+        if(r.image !== undefined){
+          if(r.image.url !== undefined){
+            listHeroes.push(r);
+          }
+        }
+        
+        }
+      }
+      
+    
+    return (
+      <div className="card">
+        {listHeroes.map(h => {
+          return(
+            <div key={h?.id}>
+              <p>Nome: {h?.name}</p>
+              <img className="heroImg" src={h?.image.url}></img>
+              <p >Publicado:{h?.biography.publisher} </p>
+              <button  className="maisInfo">Mais Informação</button>
+            </div>
+        );
+          
+        })}
+        
+      </div>
+    );
+}
